98ac579bf76b0b72c38a954c108f4c5e1f5fa4a2
AsmaaAkhtar <113789786+AsmaaAkhtar@users.noreply.github.com>
2023-06-12 22:03:54 +0500

    Add files via upload

---
+++ Assignment4/HomeScreen.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HomeScreen = ({route,navigation}) => {
+  return (
+    <View>
+      <Text>{route.params.obj2}</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor:"#b0c4de",
+  
+    }
+})
